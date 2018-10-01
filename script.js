f72eaf43c225f75a69e1f59145284a0db697e16e
Ranchino Bahadin <ranjbahadin@live.se>
2018-10-01 10:45:13 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function menuButtonClicked() {
+   var header = document.getElementById("header");
+
+   header.classList.toggle("fullscreen");
+
+}
+
+
+function scrollToTop() {
+    window.scroll({
+        top: 0,
+        behavior: "smooth",
+    });
+}
+
